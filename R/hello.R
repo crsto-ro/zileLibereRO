4743f74ce176977b31c76cd7031fbaf115551583
crsto-ro <office@crsto.ro>
2018-01-31 22:25:42 +0200

    commit pasteOrtodox()

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,49 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+# git push -u origin master
+# pentru commit la github
+
+pasteOrtodox <- function(anul,
+                         despartitor = "-",
+                         asDate = FALSE)
+{
+  A = anul %% 19
+
+  B = anul %% 4
+
+  C = anul %% 7
+
+  D = (19 * A + 15) %% 30
+
+  E = (2 * B + 4 * C + 6 * D + 6) %% 7
+
+  if (asDate == TRUE)
+  {
+    despartitor = "-"
+    if ((D + E + 4) <= 30)
+    {
+      return(as.Date.character(
+        paste0(D + E + 4, despartitor, "04", despartitor, anul),
+        "%d-%m-%Y"
+      ))
+    }
+
+
+    return(as.Date.character(
+      paste0(D + E + 4 - 30, despartitor, "05", despartitor, anul),
+      "%d-%m-%Y"
+    ))
+  }
+
+  else
+  {
+    if ((D + E + 4) <= 30)
+    {
+      return(paste0(D + E + 4, despartitor, "04", despartitor, anul))
+    }
+
+    return(paste0((D + E + 4 - 30), despartitor, "05", despartitor, anul))
+  }
 }
+
