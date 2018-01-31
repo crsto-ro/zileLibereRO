7f0af22ab460664def231f73541483b277927cd4
crsto-ro <office@crsto.ro>
2018-01-31 22:29:09 +0200

    toaleta fisier

--- R/pasteOrtodox.R
+++ R/pasteOrtodox.R
@@ -1,12 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
+# Calcul paste ortodox!
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Build and Reload Package:  'Ctrl + Shift + B'
