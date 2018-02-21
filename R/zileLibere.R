78c47d3706e484e5e214107f5b34730474ce288e
crsto-ro <office@crsto.ro>
2018-02-21 13:18:28 +0200

    adaugare vinerea mare la zilele libere din Romania incepand cu 2018

--- R/zileLibere.R
+++ R/zileLibere.R
@@ -3,15 +3,16 @@ zileLibere <- function(an = 2017)
   an = as.character(an)
   paste = pasteOrtodox(as.numeric(an), asDate = TRUE) + 1
   rusalii = paste + 49
-  
+
   zile_libere = c(
     as.Date(paste0(an, "-01-01")),
     as.Date(paste0(an, "-01-02")),
     as.Date(paste0(an, "-01-24")),
-    
+
+    vinereaMare,
     paste,
     rusalii,
-    
+
     as.Date(paste0(an, "-05-01")),
     as.Date(paste0(an, "-08-15")),
     as.Date(paste0(an, "-11-30")),
@@ -19,40 +20,46 @@ zileLibere <- function(an = 2017)
     as.Date(paste0(an, "-12-25")),
     as.Date(paste0(an, "-12-26"))
   )
-  
-  
+
+
   if (an > 2016)
   {
     zile_libere = c(zile_libere, as.Date(paste0(an, "-06-01")))
   }
-  
+
+  if (an > 2017)
+  {
+    vinereaMare = paste-3
+    zile_libere = c(zile_libere, as.Date(vinereaMare))
+  }
+
   return(sort(zile_libere))
-  
+
 }
 
 
 zileLibereIntervalAni <- function(anStart = 2005, anStop = 2050)
 {
   zileLibereInInterval <- zileLibere(an = anStart)
-  
+
   for (i in (anStart + 1):anStop)
   {
     zileLibereInInterval <- c(zileLibereInInterval, zileLibere(an = i))
-    
+
   }
-  
+
   return(zileLibereInInterval)
-  
+
 }
 
 
 workdays <-  function(iniDate, endDate, holidays) {
-  
+
   theDates = seq(from = as.Date.character(iniDate), to = as.Date.character(endDate), by = "day")
-  
+
   isHoliday = theDates %in% holidays
-  
+
   isWeekend = (as.POSIXlt(theDates)$wday) %in% (c(0, 6))
-  
+
   return (sum(!isHoliday & !isWeekend))
 }
